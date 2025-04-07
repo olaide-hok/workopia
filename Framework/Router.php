5eb3d6187a9fb87fd1742de84fe4644204086a04
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-04-07 17:34:25 +0100

    update registerRoute to recieve controller name and method.
    update route to handle controller classes and its methods

--- Framework/Router.php
+++ Framework/Router.php
@@ -14,12 +14,14 @@ class Router
      * @param string $controller
      * @return void
      */
-    public function registerRoute($method, $uri, $controller)
+    public function registerRoute($method, $uri, $action)
     {
+        list($controller, $controllerMethod) =  explode('@', $action);
         $this->routes[] = [
             'method' => $method,
             'uri' => $uri,
-            'controller' => $controller
+            'controller' => $controller,
+            'controllerMethod' => $controllerMethod
         ];
     }
 
@@ -95,7 +97,13 @@ class Router
     {
         foreach ($this->routes as $route) {
             if ($route['uri'] === $uri && $route['method'] === $method) {
-                require basePath('App/' . $route['controller']);
+                // Extract controller and controller method
+                $controller = 'App\\Controllers\\' . $route['controller'];
+                $controllerMethod = $route['controllerMethod'];
+
+                // Instantiate the controller and call the method
+                $controllerInstance = new $controller();
+                $controllerInstance->$controllerMethod();
                 return;
             }
         }
