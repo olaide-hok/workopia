be3b4383a6bb41f8f3bfc5c2ac6ddd5e151ea273
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-04-07 17:00:15 +0100

    setup custom autoloader

--- public/index.php
+++ public/index.php
@@ -1,7 +1,17 @@
 <?php
 require '../helpers.php';
-require basePath('Framework/Database.php');
-require basePath('Framework/Router.php');
+// require basePath('Framework/Database.php');
+// require basePath('Framework/Router.php');
+
+/**
+ * custom auto loader
+ */
+spl_autoload_register(function ($class) {
+    $path = basePath('Framework/' . $class . '.php');
+    if (file_exists($path)) {
+        require $path;
+    }
+});
 
 // Instantiate the router
 $router = new Router();
