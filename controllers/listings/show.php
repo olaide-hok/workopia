fb348b829c4d424467ebde5506ce4d7cd8980d16
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-04-07 16:38:00 +0100

    pass listing data to loadView function

--- controllers/listings/show.php
+++ controllers/listings/show.php
@@ -12,4 +12,6 @@ $params = [
 
 $listing = $db->query('SELECT * FROM listings WHERE id = :id', $params)->fetch();
 
-loadView('listings/show');
+loadView('listings/show', [
+    'listing' => $listing
+]);
