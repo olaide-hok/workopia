0041ee6df6b04949510486f8a850281f91418890
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-04-07 17:08:10 +0100

    require autoload pkg

--- public/index.php
+++ public/index.php
@@ -1,17 +1,16 @@
 <?php
+require __DIR__ . '/../vendor/autoload.php';
 require '../helpers.php';
-// require basePath('Framework/Database.php');
-// require basePath('Framework/Router.php');
 
 /**
  * custom auto loader
  */
-spl_autoload_register(function ($class) {
-    $path = basePath('Framework/' . $class . '.php');
-    if (file_exists($path)) {
-        require $path;
-    }
-});
+// spl_autoload_register(function ($class) {
+//     $path = basePath('Framework/' . $class . '.php');
+//     if (file_exists($path)) {
+//         require $path;
+//     }
+// });
 
 // Instantiate the router
 $router = new Router();
