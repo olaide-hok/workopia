023e871488b25a8ce29b04c4c2969702bb7cb6dd
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-04-07 15:41:58 +0100

    fetch data in object format

--- Database.php
+++ Database.php
@@ -15,8 +15,8 @@ class Database
 
         $options = [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-
+            // PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC use to fetch data as associative array
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
         ];
 
         try {
