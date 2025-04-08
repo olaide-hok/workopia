d132e0f910cb49a26bb7893b1aab7b68db9e7b55
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-04-08 12:44:31 +0100

    add session flash message in destroy method

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -151,6 +151,8 @@ class ListingController
 
         $this->db->query('DELETE FROM listings WHERE id = :id', $params);
 
+        // Set flash message
+        $_SESSION['success_message'] = 'Listing deleted successfully';
 
         redirect('/listings');
     }
