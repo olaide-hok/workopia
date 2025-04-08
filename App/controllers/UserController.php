4b53f8059a59e7d3216c513521d4f7802f5a8f94
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-04-08 15:03:24 +0100

    add Session framework
    set session in store method

--- App/controllers/UserController.php
+++ App/controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use Framework\Database;
 use Framework\Validation;
+use Framework\Session;
 
 class UserController
 {
@@ -106,6 +107,18 @@ class UserController
         ];
 
         $this->db->query('INSERT INTO users (name, email, city, state, password) VALUES (:name, :email, :city, :state, :password)', $params);
+
+        // Get new user ID
+        $userId = $this->db->conn->lastInsertId();
+
+        // Set user session
+        Session::set('user', [
+            'id' => $user->id,
+            'name' => $user->name,
+            'email' => $user->email,
+            'city' => $user->city,
+            'state' => $user->state
+        ]);
 
         redirect('/');
     }
